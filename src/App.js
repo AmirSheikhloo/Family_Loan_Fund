71969b92491d7ccef662bb7f99d6029368dbb31c
Shahryar Akhteh <sh.akhteh@gmail.com>
2024-01-08 17:07:32 +0330

    Add BudgetsContext AddBudgetModal useLocalStorage

--- src/App.js
+++ src/App.js
@@ -1,47 +1,57 @@
 import { Button, Container, Stack } from "react-bootstrap";
 import BudgetCard from "./components/BudgetCard";
+import AddBudgetModal from "./components/AddBudgetModal";
+import { useState } from "react";
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+
   return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Family Loan Fund</h1>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            width: "300px",
-            marginTop: "30px",
-          }}
-        >
-          <div style={{ display: "flex", gap: "6px" }}>
-            <Button variant="outline-success" style={{ flex: 1 }}>
-              Add Loan
-            </Button>
-            <Button variant="outline-success" style={{ flex: 1 }}>
-              Add Expense
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Family Loan Fund</h1>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "300px",
+              marginTop: "30px",
+            }}
+          >
+            <div style={{ display: "flex", gap: "6px" }}>
+              <Button variant="outline-success"  onClick={() => setShowAddBudgetModal(true)} style={{ flex: 1 }}>
+                Add Budget
+              </Button>
+              <Button variant="outline-success"  style={{ flex: 1 }}>
+                Add Expense
+              </Button>
+            </div>
+            <Button variant="outline-info" style={{ marginTop: "7px" }}>
+              Membership Fee
             </Button>
           </div>
-          <Button variant="outline-info" style={{ marginTop: "7px" }}>
-            Membership fee
-          </Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
+          <BudgetCard
+            name="Entertainment"
+            gray
+            amount={200}
+            max={1000}
+          ></BudgetCard>
         </div>
-      </Stack>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-          gap: "1rem",
-          alignItems: "flex-start",
-        }}
-      >
-        <BudgetCard
-          name="Entertainment"
-          gray
-          amount={200}
-          max={1000}
-        ></BudgetCard>
-      </div>
-    </Container>
+      </Container>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
+    </>
   );
 }
 
--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,4 +1,6 @@
 import React, { useContext, useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const BudgetsContext = React.createContext();
 
@@ -7,14 +9,40 @@ export function useBudgets() {
 }
 
 export const BudgetsProvider = ({ children }) => {
-  const [budgets, setBudgets] = useState([]);
-  const [expenses, setExpenses] = useState([]);
+  const [budgets, setBudgets] = useLocalStorage("budgets", []);
+  const [expenses, setExpenses] = useLocalStorage("expenses", []);
 
-  function getBudgetExpenses(budgetId) {}
-  function addExpense() {}
-  function addBudget() {}
-  function deleteBudget() {}
-  function deleteexpense() {}
+  function getBudgetExpenses(budgetId) {
+    return expenses.filter((expense) => expense.budgetId === budgetId);
+  }
+
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
+    });
+  }
+
+  function addBudget({ name, max }) {
+    setBudgets((prevBudgets) => {
+      if (prevBudgets.find((budget) => budget.name === name)) {
+        return prevBudgets;
+      }
+      return [...prevBudgets, { id: uuidV4(), name, max }];
+    });
+  }
+
+  function deleteBudget({ id }) {
+    //TODO: Deal with Uncategorized
+    setBudgets((prevBudgets) => {
+      return prevBudgets.filter((budget) => budget.id !== id);
+    });
+  }
+
+  function deleteExpense({ id }) {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== id);
+    });
+  }
   return (
     <BudgetsContext.Provider
       value={{
@@ -24,7 +52,7 @@ export const BudgetsProvider = ({ children }) => {
         addExpense,
         addBudget,
         deleteBudget,
-        deleteexpense,
+        deleteExpense,
       }}
     >
       {children}
