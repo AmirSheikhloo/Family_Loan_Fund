d2fe91a2075c5c6cdfda135aa46a0623c3b609af
Shahryar Akhteh <sh.akhteh@gmail.com>
2024-01-09 14:39:00 +0330

    default with new buttons

--- src/components/AddBudgetModal.js
+++ src/components/AddBudgetModal.js
@@ -1,28 +1,18 @@
-import { Button, Form, Modal } from "react-bootstrap";
-import { useRef, useState } from "react";
-import { useBudgets } from "../contexts/BudgetsContext";
-import { parse } from "uuid";
+import { Form, Modal, Button } from "react-bootstrap"
+import { useRef } from "react"
+import { useBudgets } from "../contexts/BudgetsContext"
 
 export default function AddBudgetModal({ show, handleClose }) {
-  const nameRef = useRef();
-  const maxRef = useRef();
-  const { addBudget } = useBudgets();
-  const [formattedValue, setFormattedValue] = useState("");
-
+  const nameRef = useRef()
+  const maxRef = useRef()
+  const { addBudget } = useBudgets()
   function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
     addBudget({
       name: nameRef.current.value,
-      max: parseFloat(maxRef.current.value.replace(/,/g, ""))
-    });
-    handleClose();
-  }
-
-  function handleInputChange(e) {
-    const rawValue = e.target.value.replace(/\D/g, "");
-    const formattedValue = rawValue.replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, "$&,");
-    setFormattedValue(formattedValue);
-    maxRef.current.value = rawValue;
+      max: parseFloat(maxRef.current.value),
+    })
+    handleClose()
   }
 
   return (
@@ -34,22 +24,16 @@ export default function AddBudgetModal({ show, handleClose }) {
         <Modal.Body>
           <Form.Group className="mb-3" controlId="name">
             <Form.Label>Name</Form.Label>
-            <Form.Select ref={nameRef} required>
-              <option value="Amir">Amir</option>
-              <option value="Reza">Reza</option>
-              <option value="Sima">Sima</option>
-              <option value="Ali">Ali</option>
-              <option value="Mohsen">Mohsen</option>
-            </Form.Select>
+            <Form.Control ref={nameRef} type="text" required />
           </Form.Group>
           <Form.Group className="mb-3" controlId="max">
             <Form.Label>Maximum Spending</Form.Label>
             <Form.Control
               ref={maxRef}
-              type="text"
-              value={formattedValue}
+              type="number"
               required
-              onChange={handleInputChange}
+              min={0}
+              step={0.01}
             />
           </Form.Group>
           <div className="d-flex justify-content-end">
@@ -60,5 +44,5 @@ export default function AddBudgetModal({ show, handleClose }) {
         </Modal.Body>
       </Form>
     </Modal>
-  );
+  )
 }
--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,19 +1,18 @@
-import { Button, Card, ProgressBar, Stack } from "react-bootstrap";
-import { currencyFormatter } from "../utils";
+import { Button, Card, ProgressBar, Stack } from "react-bootstrap"
+import { currencyFormatter } from "../utils"
 
 export default function BudgetCard({
-  name, 
-  amount, 
-  max, 
-  gray, 
-  hideButtons, 
+  name,
+  amount,
+  max,
+  gray,
+  hideButtons,
   onAddExpenseClick,
   onViewExpensesClick,
 }) {
-
-  const classNames = [] 
+  const classNames = []
   if (amount > max) {
-    classNames.push("bg-success", "bg-opacity-10")
+    classNames.push("bg-danger", "bg-opacity-10")
   } else if (gray) {
     classNames.push("bg-light")
   }
@@ -22,28 +21,37 @@ export default function BudgetCard({
     <Card className={classNames.join(" ")}>
       <Card.Body>
         <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
-            <div className="me-2">{name}</div>
-            <div className="d-flex align-items-baseline">
-            {currencyFormatter.format(amount)} 
+          <div className="me-2">{name}</div>
+          <div className="d-flex align-items-baseline">
+            {currencyFormatter.format(amount)}
             {max && (
               <span className="text-muted fs-6 ms-1">
-              / {currencyFormatter.format(max)}
+                / {currencyFormatter.format(max)}
               </span>
             )}
           </div>
         </Card.Title>
         {max && (
-          <ProgressBar className="rounded-pill" 
-          variant={getProgressBarVariant(amount, max)}
-          min={0}
-          max={max}
-          now={amount}
-         />
+          <ProgressBar
+            className="rounded-pill"
+            variant={getProgressBarVariant(amount, max)}
+            min={0}
+            max={max}
+            now={amount}
+          />
         )}
-        {!hideButtons && ( 
+        {!hideButtons && (
           <Stack direction="horizontal" gap="2" className="mt-4">
-              <Button variant="outline-primary" className="ms-auto" onClick={onAddExpenseClick}>Add Expense</Button>
-              <Button onClick={onViewExpensesClick} variant="outline-secondary">View Expenses</Button>
+            <Button
+              variant="outline-primary"
+              className="ms-auto"
+              onClick={onAddExpenseClick}
+            >
+              Add Expense
+            </Button>
+            <Button onClick={onViewExpensesClick} variant="outline-secondary">
+              View Expenses
+            </Button>
           </Stack>
         )}
       </Card.Body>
@@ -52,9 +60,8 @@ export default function BudgetCard({
 }
 
 function getProgressBarVariant(amount, max) {
-    const ratio = amount / max
-    if (ratio < .5) return "warning"
-    if (ratio < 1.0) return "primary"
-    if (ratio == 1.0) return "success"
-    return "danger"
+  const ratio = amount / max
+  if (ratio < 0.5) return "primary"
+  if (ratio < 0.75) return "warning"
+  return "danger"
 }
